f27c1be1ccb4b8d19ae6aadad339b2d6a31f663d
Giuli <giuli1297.gg@gmail.com>
2022-07-13 21:52:58 -0400

    Clase 3/CodigoComentado

--- clase3/asincronia.js
+++ clase3/asincronia.js
@@ -1,59 +1,65 @@
 ///Callbacks 
-//Errores
-// const fs = require('fs')
-// let x;
-// console.log("ANTES")
-// fs.readFile('modulosDelCore.js', (err, data)=>{
-//     if(err){
-//         console.log(err)
-//         return
-//     }
-//     x = data;
-//     console.log(data)
-// })
-// console.log(x)
 
-// const done = false
+const fs = require('fs')
+let x;
+console.log("ANTES")
+fs.readFile('modulosDelCore.js', (err, data)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    x = data;
+    console.log(data)
+})
+console.log(x)
 
-// const isItDoneYet = new Promise((resolve, reject)=>{
-//     if(done){
-//         resolve("Exito")
-//     }else{
-//         reject("Fallo")
-//     }
-// });
+// Proomesas
 
-// const checkIfItsDone = () => {
-//     isItDoneYet
-//         .then(ok => {
-//             console.log(ok)
-//         })
-//         .catch(err => {
-//             console.error(err)
-//         });
-// }
+const done = false
 
-// checkIfItsDone()
+const isItDoneYet = new Promise((resolve, reject)=>{
+    if(done){
+        resolve("Exito")
+    }else{
+        reject("Fallo")
+    }
+});
 
-// const fs = require('fs')
+const checkIfItsDone = () => {
+    isItDoneYet
+        .then(ok => {
+            console.log(ok)
+        })
+        .catch(err => {
+            console.error(err)
+        });
+}
 
-// const getFileConPromesa = fileName => {
-//     return new Promise((resolve, reject)=>{
-//         fs.readFile(fileName, (err, data)=>{
-//             if(err){
-//                 reject(err)
-//                 return
-//             }
-//             x = data;
-//             resolve(data);
-//     });
-// });
-// }
+checkIfItsDone()
 
-// getFileConPromesa('modulosDelCore.js')
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+// Promisifying
 
+const fs = require('fs')
+
+const getFileConPromesa = fileName => {
+    return new Promise((resolve, reject)=>{
+        fs.readFile(fileName, (err, data)=>{
+            if(err){
+                reject(err)
+                return
+            }
+            x = data;
+            resolve(data);
+    });
+});
+}
+
+getFileConPromesa('modulosDelCore.js')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+// Async / Await
+    
 const doSomethingAsync = () => {
     return new Promise(resolve=>{
         setTimeout(()=>resolve("Exito"), 3000);
@@ -62,7 +68,7 @@ const doSomethingAsync = () => {
 
 const doSomething = async () => {
     console.log('antes')
-    console.log(doSomethingAsync());
+    console.log(await doSomethingAsync());
     console.log('despues')
 }
 
--- clase3/modulosDelCore.js
+++ clase3/modulosDelCore.js
@@ -1,37 +1,46 @@
+//Modulo OS
+
 const os = require('os')
 
-// console.log(os.totalmem())
-// console.log(os.freemem())
+console.log(os.totalmem())
+console.log(os.freemem())
 
-// console.log(os.networkInterfaces())
+console.log(os.networkInterfaces())
 
-// const fs = require('fs/promises')
-// console.time()
-// async function exampleFS(){
-//     const data = await fs.readFile('asincronia.js', { encoding: 'utf8' });
-//     const data2 = await fs.readdir(".");
-//     console.log("==============")
-//     // console.log(data);
-//     console.log("==============")
-//     await fs.writeFile('test.js', 'console.log("tests")')
-//     await fs.appendFile('test.js', '\nlet a = 5;')
-//     await fs.unlink('test.js')
-// }
+//Modulo FileSystem
 
-// exampleFS()
-// console.timeEnd()
+const fs = require('fs/promises')
+console.time()
+async function exampleFS(){
+    const data = await fs.readFile('asincronia.js', { encoding: 'utf8' });
+    const data2 = await fs.readdir(".");
+    console.log("==============")
+    // console.log(data);
+    console.log("==============")
+    await fs.writeFile('test.js', 'console.log("tests")')
+    await fs.appendFile('test.js', '\nlet a = 5;')
+    await fs.unlink('test.js')
+}
 
-// const http = require('http')
+exampleFS()
+//Modulo console
+console.timeEnd()
 
-// const server = http.createServer((req, res)=>{
-//     res.statusCode = 200
-//     res.setHeader('Content-Type', 'text/html')
-//     res.end("<h3>Hola</h3>")
-// });
 
-// server.listen(3000, ()=>{
-//     console.log("Server arriba");
-// })
+//Modulo HTTP
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end("<h3>Hola</h3>")
+});
+
+server.listen(3000, ()=>{
+    console.log("Server arriba");
+})
+
+//Modulo Process
 
 let a = process.argv
 console.log(a)
